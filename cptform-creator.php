44128e0a74c3e87ac225eef68e2ce9fc2d47ac02
John Overall <38515654+Ahkonsu@users.noreply.github.com>
2025-06-11 15:53:44 -0700

    Update cptform-creator.php
    quick fix

--- cptform-creator.php
+++ cptform-creator.php
@@ -22,9 +22,9 @@ require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $myUpdateChecker = PucFactory::buildUpdateChecker(
-	'https://github.com/Ahkonsu/wpproatoz-elementor-query-string-dynamic-tag/',
+	'https://github.com/Ahkonsu/wpproatoz-cptform-creator/',
 	__FILE__,
-	'wpproatoz-elementor-query-string-dynamic-tag'
+	'wpproatoz-cptform-creator'
 );
 
 //Set the branch that contains the stable release.
